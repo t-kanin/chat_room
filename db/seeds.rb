0098fe3e833768d09836a44948e1915fef32f55d
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-08-13 00:18:20 +0700

    add index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#create users
+
+john  = User.create(username: "John", password: "password")
+smith = User.create(username: "smith", password: "password")
+joe   = User.create(username: "Joe", password: "password")
+jaah  = User.create(username: "Jaah", password: "password")
+
+msg_1  = Message.create(body: "Hello smith", user: john)
+msg_2  = Message.create(body: "What do you want to do today?", user: john)
+msg_3  = Message.create(body: "Sup, john", user: smith)
+msg_4  = Message.create(body: "Anyone want some coffee?", user: joe)
+msg_5  = Message.create(body: "Give me one, pls !!", user: jaah)
