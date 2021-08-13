177f7b8c20415d963a90b411df1e8ad6975d1acb
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-08-13 18:37:27 +0700

    adding authentication + session routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,18 @@ class SessionsController < ApplicationController
     def new 
     end 
 
+    def create 
+        @user = User.find_by(username: params[:username])
+        if @user.nil? 
+            flash.now.alert = 'Invalid username'
+            render :new
+        elsif @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect_to root_path
+        else 
+            flash.now.alert = 'Invalid password'
+            render :new
+        end 
+    end 
+
 end 
