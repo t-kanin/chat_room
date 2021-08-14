e8feda227cc7e5c0ea595653ae149b688ec05333
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-08-14 21:52:24 +0700

    realtime chat with action cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,13 +5,19 @@ class MessagesController < ApplicationController
         # association between the login user and message will generate automatically
         message = current_user.messages.build(message_params)
         if message.save 
-            redirect_to root_path
-        end 
+            # this result in refresh
+            # redirect_to root_path
+            ActionCable.server.broadcast('chatroom_channel', message.body)
+        end
     end 
 
     private 
 
     def message_params 
         params.require(:message).permit(:body)
+    end 
+
+    def message_render(message)
+        render(partial: 'message', locals: {message: message}) 
     end 
 end 
