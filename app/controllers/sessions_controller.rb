f1bf09821e55a5c075e403527b5777cbec6da279
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-08-14 16:59:56 +0700

    adding destroy session for logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,24 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-        @user = User.find_by(username: params[:username])
+        @user = User.find_by(username: params[:username].downcase)
         if @user.nil? 
-            flash.now.alert = 'Invalid username'
+            flash.now[:error] = 'Invalid username'
             render :new
         elsif @user.authenticate(params[:password])
             session[:user_id] = @user.id 
+            flash[:success] = "Login successfully"
             redirect_to root_path
         else 
-            flash.now.alert = 'Invalid password'
+            flash.now[:error] = 'Invalid password'
             render :new
         end 
+    end 
+
+    def destroy 
+        session.delete(:user_id)
+        flash[:notice] = "Logout successfully" 
+        redirect_to login_path
     end 
 
 end 
